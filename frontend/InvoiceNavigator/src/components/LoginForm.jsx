ced5073099938dd4b7d80c78ed1f50194c0e2870
Mauricio650 <mauricioibanez650@gmail.com>
2025-07-05 15:24:26 -0500

    prueba

--- frontend/InvoiceNavigator/src/components/LoginForm.jsx
+++ frontend/InvoiceNavigator/src/components/LoginForm.jsx
@@ -53,6 +53,7 @@ export function LoginForm () {
   }
 
   return (
+    <div>Joderr tio</div>
     <section className='w-screen h-screen flex justify-center items-center bg-sky-800'>
       <article className='bg-sky-950 flex flex-col justify-center items-center shadow-xl/20 shadow-sky-50/100 p-5 text-white font-mono border-1 border-sky-900 rounded-md'>
         <header className='flex justify-center items-center flex-col'>
