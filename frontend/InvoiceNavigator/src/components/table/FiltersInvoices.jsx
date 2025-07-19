1b650813079433372b602fbfeaef8dba84f559b6
Mauricio650 <mauricioibanez650@gmail.com>
2025-07-18 23:47:41 -0500

    reset filters button and filters Invoices in rows

--- frontend/InvoiceNavigator/src/components/table/FiltersInvoices.jsx
+++ frontend/InvoiceNavigator/src/components/table/FiltersInvoices.jsx
@@ -1,5 +1,9 @@
 import { useId } from 'react'
 import { BtnClassic } from '../BtnClassic'
+import { useInvoices } from '../../hooks/invoices/useInvoices'
+import { BiReset } from 'react-icons/bi'
+import { useFetchInvoices } from '../../hooks/fetchs/useFetchInvoices'
+import { useRef } from 'react'
 
 export function FiltersInvoices () {
   const idInputCompany = useId()
@@ -7,6 +11,9 @@ export function FiltersInvoices () {
   const idSelectStatus = useId()
   const idInputDateFrom = useId()
   const idInputDateTo = useId()
+  const formRef = useRef(null)
+  const { updateInvoicesData } = useInvoices()
+  const { fetchData } = useFetchInvoices()
 
   const handledSubmit = async (e) => {
     e.preventDefault()
@@ -18,7 +25,6 @@ export function FiltersInvoices () {
       }
       return acc
     }, {})
-
     try {
       const res = await fetch('http://localhost:4000/home/filterInvoices', {
         credentials: 'include',
@@ -29,16 +35,25 @@ export function FiltersInvoices () {
         body: JSON.stringify(filteredData)
       })
       const response = await res.json()
-      console.log(response)
+      const data = response.invoices.map(i => ({
+        ...i,
+        id: i._id
+      }))
+      updateInvoicesData({ data })
     } catch (error) {
       console.error('Internal server error', error)
     }
   }
 
+  const handleClear = () => {
+    fetchData()
+    formRef.current.reset()
+  }
+
   return (
     <article className='bg-white/60 h-full backdrop-blur-md p-4 rounded-xl shadow-lg'>
 
-      <form onSubmit={handledSubmit} className='h-full flex flex-col gap-3 justify-around items-center'>
+      <form ref={formRef} onSubmit={handledSubmit} className='h-full flex flex-col gap-3 justify-around items-center'>
         <div className='w-full flex gap-5 justify-around items-center'>
           <div>
             <label htmlFor={idInputCompany} className='block'>
@@ -70,7 +85,7 @@ export function FiltersInvoices () {
             <label htmlFor={idInputDateFrom} className='block'>
               From
             </label>
-            <input type='date' name='from' id={idInputDateFrom} className='max-w-sm p-1 bg-gray-100 ring-2 ring-gray-500 rounded-xl focus:outline-blue-800 focus:bg-gray-400 focus:text-white' placeholder='Tesla enterprise' />
+            <input required type='date' name='from' id={idInputDateFrom} className='max-w-sm p-1 bg-gray-100 ring-2 ring-gray-500 rounded-xl focus:outline-blue-800 focus:bg-gray-400 focus:text-white' placeholder='Tesla enterprise' />
           </div>
 
           <div>
@@ -81,10 +96,18 @@ export function FiltersInvoices () {
           </div>
         </div>
 
-        <aside>
+        <aside className='flex gap-5 justify-center items-center'>
           <BtnClassic>
             Search
           </BtnClassic>
+          <div
+            onClick={handleClear} className='font-medium text-white cursor-pointer
+     bg-sky-600 rounded-sm px-2 ring-2 ring-sky-50 border-b-4 border-l-3 border-r-2 border-t-2 border-transparent
+    hover:bg-sky-700 hover:border-sky-950 shadow-lg shadow-sky-500/50 hover:shadow-sky-50 transition-all ease-linear duration-200
+    '
+          >
+            <BiReset size={22} />
+          </div>
         </aside>
 
       </form>
--- frontend/InvoiceNavigator/src/hooks/fetchs/useFetchInvoices.js
+++ frontend/InvoiceNavigator/src/hooks/fetchs/useFetchInvoices.js
@@ -2,26 +2,26 @@ import { useEffect } from 'react'
 import { useInvoices } from '../invoices/useInvoices'
 export function useFetchInvoices () {
   const { updateInvoicesData, setLoading, setError } = useInvoices()
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await fetch('http://localhost:4000/home/invoices', {
-          credentials: 'include'
-        })
-        const json = await res.json()
-        const data = json.map(i => ({
-          ...i,
-          id: i._id
-        }))
-        updateInvoicesData({ data })
-      } catch (err) {
-        console.error('Error:', err)
-        setError(true)
-      } finally {
-        setLoading(false)
-      }
-    }
 
+  const fetchData = async () => {
+    try {
+      const res = await fetch('http://localhost:4000/home/invoices', {
+        credentials: 'include'
+      })
+      const json = await res.json()
+      const data = json.map(i => ({
+        ...i,
+        id: i._id
+      }))
+      updateInvoicesData({ data })
+    } catch (err) {
+      console.error('Error:', err)
+      setError(true)
+    } finally {
+      setLoading(false)
+    }
+  }
+  useEffect(() => {
     fetchData()
 
     const intervalID = setInterval(fetchData, 3600000)
@@ -30,4 +30,6 @@ export function useFetchInvoices () {
       clearInterval(intervalID)
     }
   }, [])
+
+  return { fetchData }
 }
